6263921b627df26bfb0faf4e45ab6fa0184994ff
magbha <111670119+magbha@users.noreply.github.com>
2022-10-21 15:35:23 +0000

    Adding css checkpoint

---
+++ is.js
@@ -0,0 +1,66 @@
+// change the function parameters if you need  
+// function test(y) { 
+//     var sum = 0;
+//     var i= 0;
+    
+//     while (i < y.length) {
+//         sum += y[i]
+//         i += 1
+//     }
+       
+//     return sum
+//     }
+    
+//    // change the function parameters if you need  
+
+// function test(arr) { 
+//     let i=0
+//     let prod=1
+//     do{
+//     prod *= arr[i]
+//     i++;
+//     }
+//     while(i < arr.length)
+//     return prod
+//     }
+
+// change the function parameters if you need  
+// function test(arr) { 
+    
+//     for (let i=0  ; i< arr.length; i++){
+//     arr[i] *= arr[i]
+//     arr.push[arr[i]]
+//      }
+//   return arr
+// }
+
+// function test(day) {
+//     switch (day) {
+//       case  "monday" : return 1
+//       break
+//       case "tuesday" : return 2
+//       break
+//       case "wednesday" : return  3
+//        break
+//       case "thursday" : return 4
+//        break
+//       case "friday" : return  5
+//       break
+//         case "saturday" : return 6
+//       break
+//         case "sunday" : return 7
+//       break
+
+//       default:return 'please enter a valid day'
+//     }
+// }
+
+
+// change the function parameters if you need  
+function test(arr) { 
+  // Write your code below this line  
+ arr.pop();
+ arr.unshift(0);
+ 
+    return arr
+ }
